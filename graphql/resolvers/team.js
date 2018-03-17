6365d29d65f6b990c31e78c67fe789773f7323a3
Ryan Yogan <ryanyogan@gmail.com>
2018-03-17 13:58:05 -0700

    created small wrapper function to apply permission on the rootResolver level (do not go talking to the DB for no reason, bubble up errors)

--- graphql/resolvers/team.js
+++ graphql/resolvers/team.js
@@ -1,20 +1,22 @@
 import { formatErrors } from '../../lib/formatErrors';
+import { requiresAuth } from '../../lib/permissions';
 
 export default {
   Mutation: {
-    createTeam: async (parent, args, { models, user }) => {
-      try {
-        await models.Team.create({ ...args, owner: user.id });
-        return {
-          ok: true,
-        };
-      } catch (error) {
-        console.error(error); // eslint-disable-line
-        return {
-          ok: false,
-          errors: formatErrors(error, models),
-        };
-      }
-    },
+    createTeam: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Team.create({ ...args, owner: user.id });
+          return {
+            ok: true,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            errors: formatErrors(error, models),
+          };
+        }
+      },
+    ),
   },
 };
